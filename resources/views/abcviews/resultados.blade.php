828e7d01053628811fd0f0d4c8b56b058e90469b
nicaraguaeli <moncadaeli567@gmail.com>
2020-04-13 08:34:15 -0600

    buscar

--- resources/views/abcviews/resultados.blade.php
+++ resources/views/abcviews/resultados.blade.php
@@ -3,7 +3,7 @@
 
 	<h3 class="mt-5">Hemos encontrado <span class="badge badge-info">{{$count}}</span> resultados sobre: {{$buscar}} </h3>
 	<div class="row mt-5">
-		@foreach($notas as $global)
+		@foreach($notas as $global_nota)
 		<div class="col-lg-3 wow bounceInLeft " data-wow-delay="0.{{$i+=1}}s">
 			@include('layouts.partial.nota')
 		</div>
