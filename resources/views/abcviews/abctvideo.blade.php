c3496fb2d77f850eeb0e039fa21bf074c88f9092
nicaraguaeli <moncadaeli567@gmail.com>
2020-04-27 14:26:42 -0600

    vid

--- resources/views/abcviews/abctvideo.blade.php
+++ resources/views/abcviews/abctvideo.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.home')
 @section('etiquetas')
-  <meta property="og:url"           content="http://www.radioabcstereo.com/nota/{{$video->id}}_{{Str::slug($video->titulo,'-')}}" />
+  <meta property="og:url"           content="http://www.radioabcstereo.com/abctvsearch/{{$video->id}}_{{Str::slug($video->titulo,'-')}}" />
   <meta property="og:type"          content="website" />
   <meta property="og:title"         content="{{$video->titulo}}" />
   <meta property="og:description"   content="{{$video->descripcion}}" />
@@ -69,6 +69,20 @@
 				</div>
 			
 			</div>
+			<h4 class="mt-3"><span class="badge badge-primary">Comparte esta noticia<i class="fas fa-share-alt text-white ml-1"></i></span></h4>
+     <div class="d-flex">
+       <div class="mr-2"><div class="fb-share-button" 
+    data-href="http://www.radioabcstereo.com/abctvsearch/{{$video->id}}_{{Str::slug($video->titulo,'-')}}"  
+    data-layout="button_count">
+  </div></div>
+       <div class="mr-2">
+         <a style="border-radius: 3px; background: #1da1f2;" target="_blank " href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button  text-white pl-2 pr-2" data-show-count="true">Tweet</a>
+       </div>
+       <div class="mr-2"><a style="border-radius: 3px;" class="badge-info text-white pl-2 pr-2 bg-dark" href="mailto:?subject=Noticia&amp;body=Noticia ABC http://www.radioabcstereo.com/abctvsearch/{{$video->id}}_{{Str::slug($video->titulo,'-')}}"
+   title="Compartir por correo">
+  <i class="fal fa-envelope mr-1" style="font-size: 12px;"></i><span>Email</span>
+</a></div>
+     </div>
 
 			<div class="mt-5 ">
 				<h4>También te pueden interesar videos sobre:  @foreach($tipos as $tipo)
@@ -76,7 +90,19 @@
 					@endforeach
 				</h4>
 			</div>
+   
+   <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
 
+
+
+               
 			<script>
 	 $(function() {
     
